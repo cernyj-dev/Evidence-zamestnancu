c336259447ba5d4e482798a69f3f9d0e5f0abd07
Jakub Černý <cernyj90@fit.cvut.cz>
2024-10-20 11:32:51 +0200

    Added employee id to the corresponding accounts

--- src/Database/Account.php
+++ src/Database/Account.php
@@ -7,6 +7,7 @@ use DateTimeImmutable;
 class Account
 {
     public function __construct(
+        public int $employee_id,
         public string $name,
         public string $type,
         private ?DateTimeImmutable $expiration = null
