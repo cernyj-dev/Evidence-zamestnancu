f4330bcec6de8aad9cdee9deefdf1e4479f5be9c
Jakub Černý <cernyj90@fit.cvut.cz>
2024-10-20 11:33:32 +0200

    Given employee id to account argument

--- src/Database/Database.php
+++ src/Database/Database.php
@@ -8,6 +8,7 @@ class Database
 
     public function __construct()
     {
+        $id_counter = 0;
         $this->employees[] = new Employee(
             "Daniel Zbyněk",
             "daniel.zbynek@example.com",
@@ -16,9 +17,9 @@ class Database
             "Technický autor zjednodušující složité myšlenky.",
             "159 753 486",
             ["Technical Writer"],
-            [new Account("TechWriter", "username/password", new \DateTimeImmutable("2026-05-01"))]
+            [new Account($id_counter,"TechWriter", "username/password", new \DateTimeImmutable("2026-05-01"))]
         );
-
+        $id_counter++;
 
         $this->employees[] = new Employee(
             "Jane Smith",
@@ -28,9 +29,9 @@ class Database
             "Projektový manažer s talentem pro organizaci.",
             "987 654 321",
             ["Project Manager"],
-            [new Account("User", "username/password", new \DateTimeImmutable("2025-06-01"))]
+            [new Account($id_counter,"User", "username/password", new \DateTimeImmutable("2025-06-01"))]
         );
-
+        $id_counter++;
         // Adding additional employees
         $this->employees[] = new Employee(
             "Aneta Branická",
@@ -40,9 +41,9 @@ class Database
             "Marketingová specialistka zaměřená na digitální strategie.",
             "123 654 987",
             ["Marketing Specialist"],
-            [new Account("Marketing", "username/password", new \DateTimeImmutable("2025-05-01"))]
+            [new Account($id_counter,"Marketing", "username/password", new \DateTimeImmutable("2025-05-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Mikuláš Novotný",
             "mikulass.novotny@example.com",
@@ -51,9 +52,9 @@ class Database
             "Expert na finance se silným analytickým zázemím.",
             "654 321 987",
             ["Finance Expert"],
-            [new Account("Finance", "username/password", new \DateTimeImmutable("2025-03-01"))]
+            [new Account($id_counter,"Finance", "username/password", new \DateTimeImmutable("2025-03-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Richard Prosta",
             "richard.prosta@example.com",
@@ -62,9 +63,9 @@ class Database
             "Softwarový vývojář s vášní pro nové technologie.",
             "321 654 987",
             ["Software Developer"],
-            [new Account("Dev", "username/password", new \DateTimeImmutable("2025-04-01"))]
+            [new Account($id_counter,"Dev", "username/password", new \DateTimeImmutable("2025-04-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Nikola Vosocká",
             "nikola.vosocka@example.com",
@@ -73,9 +74,9 @@ class Database
             "HR specialistka oddaná rozvoji zaměstnanců.",
             "789 654 321",
             ["HR Specialist"],
-            [new Account("HR", "username/password", new \DateTimeImmutable("2025-07-01"))]
+            [new Account($id_counter,"HR", "username/password", new \DateTimeImmutable("2025-07-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Kvido Pelich",
             "kvido.pelich@example.com",
@@ -84,9 +85,9 @@ class Database
             "Obchodní zástupce s vynikajícími vyjednávacími dovednostmi.",
             "456 789 123",
             ["Sales Representative"],
-            [new Account("Sales", "username/password", new \DateTimeImmutable("2025-08-01"))]
+            [new Account($id_counter,"Sales", "username/password", new \DateTimeImmutable("2025-08-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Michal Pohlreich",
             "michal.pohlreich@example.com",
@@ -95,9 +96,9 @@ class Database
             "Manažer zákaznického servisu zaměřený na spokojenost klientů.",
             "147 258 369",
             ["Customer Service Manager"],
-            [new Account("CS", "username/password", new \DateTimeImmutable("2025-09-01"))]
+            [new Account($id_counter,"CS", "username/password", new \DateTimeImmutable("2025-09-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Petr Dobriš",
             "petr.dobris@example.com",
@@ -106,9 +107,9 @@ class Database
             "Koordinátor logistiky se silným organizačním zázemím.",
             "963 852 741",
             ["Logistics Coordinator"],
-            [new Account("Logistics", "username/password", new \DateTimeImmutable("2025-10-01"))]
+            [new Account($id_counter,"Logistics", "username/password", new \DateTimeImmutable("2025-10-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Lucie Fialová",
             "lucie.fialova@example.com",
@@ -117,9 +118,9 @@ class Database
             "Grafická designérka s dobrým okem pro estetiku.",
             "258 369 147",
             ["Graphic Designer"],
-            [new Account("Design", "username/password", new \DateTimeImmutable("2025-11-01"))]
+            [new Account($id_counter,"Design", "username/password", new \DateTimeImmutable("2025-11-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Tereza Rezková",
             "tereza.rezkova@example.com",
@@ -128,9 +129,9 @@ class Database
             "Tvůrkyně obsahu zaměřená na poutavé vyprávění.",
             "852 741 963",
             ["Content Creator"],
-            [new Account("Content", "username/password", new \DateTimeImmutable("2025-12-01"))]
+            [new Account($id_counter,"Content", "username/password", new \DateTimeImmutable("2025-12-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Roman Malý",
             "roman.maly@example.com",
@@ -139,9 +140,9 @@ class Database
             "Manažer operací se specializací na optimalizaci efektivity.",
             "654 123 789",
             ["Operations Manager"],
-            [new Account("Ops", "username/password", new \DateTimeImmutable("2026-01-01"))]
+            [new Account($id_counter,"Ops", "username/password", new \DateTimeImmutable("2026-01-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Tomáš Černý",
             "tomas.cerny@example.com",
@@ -150,9 +151,9 @@ class Database
             "Specialistka IT podpory zajišťující plynulý chod operací.",
             "321 987 654",
             ["IT Support"],
-            [new Account("Support", "username/password", new \DateTimeImmutable("2026-02-01"))]
+            [new Account($id_counter,"Support", "username/password", new \DateTimeImmutable("2026-02-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Erik Polora",
             "erik.polora@example.com",
@@ -161,9 +162,9 @@ class Database
             "Webový vývojář se zájmem o frontendové technologie.",
             "654 789 321",
             ["Web Developer"],
-            [new Account("WebDev", "username/password", new \DateTimeImmutable("2026-03-01"))]
+            [new Account($id_counter,"WebDev", "username/password", new \DateTimeImmutable("2026-03-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Nikola Mizikovic",
             "nikola.mizikovic@example.com",
@@ -172,9 +173,9 @@ class Database
             "Analytik dat se silným zaměřením na rozhodování na základě dat.",
             "789 321 654",
             ["Data Analyst"],
-            [new Account("Data", "username/password", new \DateTimeImmutable("2026-04-01"))]
+            [new Account($id_counter,"Data", "username/password", new \DateTimeImmutable("2026-04-01"))]
         );
-
+        $id_counter++;
         $this->employees[] = new Employee(
             "Petr Novák",
             "petr.novak@priklad.cz",
@@ -183,8 +184,8 @@ class Database
             "Petr je spolehlivý IT pracovník, který se vždy snaží najít efektivní řešení problémů. Kromě své odbornosti v oblasti správy systémů je také vášnivým rybářem, což mu pomáhá udržovat rovnováhu mezi prací a volným časem. Kolegové na něj mohou vždy spoléhat, protože je precizní a rychle reaguje na jakýkoli technický problém. Jeho schopnost přemýšlet mimo rámec běžných postupů z něj dělá nepostradatelného člena týmu. Ať už se jedná o správu serverů nebo o pomoc s uživatelskými dotazy, vždy přistupuje ke své práci s maximální péčí a profesionalitou.",
             "123 456 789",
             ["IT specialista", "Projektový manažer"],
-            [new Account("Admin", "karta", new \DateTimeImmutable("2028-01-01")),
-                new Account("IT Podpora", "username/password", new \DateTimeImmutable("2025-01-01"))]
+            [new Account($id_counter,"Admin", "karta", new \DateTimeImmutable("2028-01-01")),
+                new Account($id_counter,"IT Podpora", "username/password", new \DateTimeImmutable("2025-01-01"))]
         );
 
     }
