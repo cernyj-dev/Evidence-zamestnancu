fafdc844f540375d205a79be54e0d41876373817
Jakub Černý <cernyj90@fit.cvut.cz>
2024-10-28 15:43:53 +0100

    Used make:entity on Account, Role, Employee

---
+++ src/Entity/Account.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AccountRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AccountRepository::class)]
+class Account
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $employee_id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $expiration = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmployeeId(): ?int
+    {
+        return $this->employee_id;
+    }
+
+    public function setEmployeeId(int $employee_id): static
+    {
+        $this->employee_id = $employee_id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getExpiration(): ?string
+    {
+        return $this->expiration;
+    }
+
+    public function setExpiration(string $expiration): static
+    {
+        $this->expiration = $expiration;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Employee.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EmployeeRepository::class)]
+class Employee
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image_url = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $office_location = null;
+
+    #[ORM\Column(length: 1500, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phone = null;
+
+    #[ORM\Column(type: 'array', nullable: true)]
+    private ?array $role_ids = null;
+
+    #[ORM\Column(type: 'array', nullable: true)]
+    private ?array $account_ids = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->image_url;
+    }
+
+    public function setImageUrl(string $image_url): static
+    {
+        $this->image_url = $image_url;
+
+        return $this;
+    }
+
+    public function getOfficeLocation(): ?string
+    {
+        return $this->office_location;
+    }
+
+    public function setOfficeLocation(?string $office_location): static
+    {
+        $this->office_location = $office_location;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(string $phone): static
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getRoles(): ?array
+    {
+        return $this->role_ids;
+    }
+
+    public function setRoles(?array $role_ids): static
+    {
+        $this->role_ids = $role_ids;
+
+        return $this;
+    }
+
+    public function getAccounts(): ?array
+    {
+        return $this->account_ids;
+    }
+
+    public function setAccounts(?array $account_ids): static
+    {
+        $this->account_ids = $account_ids;
+
+        return $this;
+    }
+}
