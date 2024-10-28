06adc235225363989d2137306634e5ef0b255b20
Jakub Černý <cernyj90@fit.cvut.cz>
2024-10-28 18:46:06 +0100

    Role wont have an array of employees

--- src/Entity/Role.php
+++ src/Entity/Role.php
@@ -16,10 +16,6 @@ class Role
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(type: 'array', nullable: true)]
-    private ?array $employee_ids = null;
-
-
     public function getId(): ?int
     {
         return $this->id;
@@ -33,18 +29,6 @@ class Role
     public function setName(string $name): static
     {
         $this->name = $name;
-
-        return $this;
-    }
-
-    public function getRolesEmployees(): ?array
-    {
-        return $this->employee_ids;
-    }
-
-    public function setRolesEmployees(?array $employee_ids): static
-    {
-        $this->$employee_ids = $employee_ids;
 
         return $this;
     }
