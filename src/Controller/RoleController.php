c640f3306ef88bc978a2ce165136c2e1a83ad207
Jakub Černý <cernyj90@fit.cvut.cz>
2024-11-07 19:53:00 +0100

    Created form for creating and editing Roles

--- src/Controller/RoleController.php
+++ src/Controller/RoleController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
-use App\Entity\Employee;
-
+use App\Entity\Role;
+use App\Form\RoleType;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\RoleRepository;
-
+use App\Operation\RoleOperation;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/roles')]
 class RoleController extends AbstractController
 {
     public function __construct(
-        private RoleRepository $roleRepository
+        private RoleRepository $roleRepository,
+        private RoleOperation $roleOperation
     ){}
     #[Route('/', name: 'app_employee_roles')]
     public function roleDisplay(): Response
@@ -23,6 +26,25 @@ class RoleController extends AbstractController
         return $this->render('roles/roles.html.twig', [
             'title' => "Všechny role",
             'all_roles' => $all_roles,
+        ]);
+    }
+    #[Route('/create', name: 'app_role_create')]
+    #[Route('/{id}/edit', name: 'app_role_edit', requirements: ['id' => '\d+'])]
+    public function form(?Role $role, Request $request): Response
+    {
+        $form = $this->createForm(RoleType::class, $role);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $id = $this->roleOperation->store($form->getData());
+
+            return $this->redirectToRoute('app_employee_roles');
+        }
+
+        return $this->render('roles/role_form.html.twig', [
+            'title' => $role ? 'Upravit roli' : 'Nová role',
+            'form' => $form->createView(),
+            'button_text' => $role ? 'Upravit' : 'Vytvořit'
         ]);
     }
 }
---
+++ src/Form/RoleType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Role;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RoleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Název role',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'This field cannot be empty']),
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Role::class,
+        ]);
+    }
+}
